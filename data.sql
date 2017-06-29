ee7fb5f5d8a6fa948333d6d9dbf253bd9e42dc9f
Sven Bohm <bohms@msu.edu>
2017-06-29 15:59:31 -0400

    actually test the sql script and add fixes

--- data.sql
+++ data.sql
@@ -1,8 +1,11 @@
-create table cannoncial (format text)
-copy cannoncial from '/Users/bohms/date-formats//cannonical.csv' DELIMITER ',' CSV;
-create table count (id int, site text, format text, count int)
+-- file path's will likely need to be adjusted
+create table cannonical (format text);
+copy cannonical from '/Users/bohms/date-formats/cannonical.csv' DELIMITER ',' CSV;
+create table counts (id int, site text, format text, count int);
 copy counts from '/Users/bohms/date-formats/formats-by-site.csv' DELIMITER ',' CSV;
 
 \f ,
 \a
-select site, counts.format, count, case when cannoncial.format is null then 'false' else 'true' end as preferred from counts left join cannoncial on counts.format = cannoncial.format;
+select site, counts.format, count, case when cannonical.format is null then 'false' else 'true' end as preferred from counts left join cannonical on counts.format = cannonical.format;
+drop table cannonical;
+drop table counts;
